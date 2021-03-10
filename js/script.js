9c59b07d676919db4aebf6db49bee6b000ed24e0
03h7 <t.merlay@gmail.com>
2021-03-10 18:25:05 +0100

    navbar

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*
+
+$(document).ready(function(){
+    $(".name").hover(
+        function(){$('.hidden_name').fadeIn(100);},
+        function(){$('.hidden_name').fadeOut(100);}
+    );
+
+    
+    var col = "#613dc1"
+
+    $(".work").hover(
+        function(){$('.work').css("color", col);},
+        function(){$('.work').css("color", "inherit");}
+    );
+    $(".exp").hover(
+        function(){$('.exp').css("color", col);},
+        function(){$('.exp').css("color", "inherit");}
+    );
+    $(".about").hover(
+        function(){$('.about').css("color", col);},
+        function(){$('.about').css("color", "inherit");}
+    );
+    
+});
+
+*/
